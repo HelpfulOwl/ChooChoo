c2be6dffd1fc86fdb1b37c51b56be7ff4c69be5c
Elyam <gregory.j.may@gmail.com>
2018-10-22 14:08:25 -0400

    updated

--- assets/JS/choochoo.js
+++ assets/JS/choochoo.js
@@ -1,5 +1,5 @@
 console.log("Your src is sauce!");
-
+var time = moment().format('h:mm');
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyAHXThonIRXQJRsKC5_OE_R1QxYp4yjwuY",
@@ -16,9 +16,6 @@ var db = firebase.database();
 
 db.ref("Awesome Sauce!");
 
-
-
-
 var trainTime = [
         {
             name: "Greg",
@@ -28,40 +25,63 @@ var trainTime = [
         }
 ]
 
-dataAdd();
-
-
 function dataAdd(){
-  var dbref=db.ref("Awesome Sauce!").set(trainTime); //adds object to database.
+  db.ref("Awesome Sauce!").set(trainTime); //adds object to database.
+  console.log('ADDED TO FIREBASE: ',trainTime);
 };
 
+dataAdd();
+retrieve();
 $("#add").on("click",function(){
+    $("#table").empty();
     var fname = $("#inputName").val();
     var fdest = $("#inputDestination").val();
     var ffirst = $("#inputFirst").val();
     var ffreq = $("#inputFrequency").val();
-
-    const newTrain = {
+    var newTrain = {
         name: fname,
         destination: fdest,
         firstTime: ffirst,
-        frequency: ffreq
+        frequency: ffreq,
     }
 
     trainTime.push(newTrain);
-    console.log (trainTime);
-    dataAdd()
-})
-
+    
+    dataAdd();
+    retrieve();
+    
+   
+});
+function retrieve(){
 db.ref().on("child_added",function(snapshot){
-    $("#table").empty();
+    console.log("child_added works!");
     for (var i = 0; i < trainTime.length; i++){
-        let tName = snapshot.val()[i].name;
-        let tDest = snapshot.val()[i].destination;
-        let tTime = snapshot.val()[i].firstTime;
-        let tFreq = snapshot.val()[i].frequency;
-        
-        $("#cTable > tbody").append("<tr><td>"+ tName + "</td><td>"+tDest+"</td><td>"+tTime+"</td><td>"+tFreq+" minutes"+"</td>");
+        var tName = snapshot.val()[i].name;
+        var tDest = snapshot.val()[i].destination;
+        var tTime = snapshot.val()[i].firstTime;
+        var tFreq = snapshot.val()[i].frequency;
+        var tDepart = timeDepart(tFreq);
+
+        $("#table").append(`<tr><td>${tName}</td><td>${tDest}
+            </td><td>${tTime}</td><td>${tFreq} minutes</td><td>${tDepart}</td></tr>`);
     };
 });
- 
+
+function timeDepart(freq){
+    var time = moment().format('H:mm');
+    var hours = moment().format('H');
+    console.log('HOURS: ', hours);
+    var minutes = moment().format('mm');
+    var hToSec = (hours*60)*60;
+    console.log('HOURS TO SECONDS: ',hToSec);
+    var mToSec = minutes*60;
+    var TotalSec = hToSec + mToSec;
+    var freqToSec = freq*60;
+    var dSec = (TotalSec + freqToSec);
+    var duration = moment.duration(dSec, 'seconds');
+    var formatted = duration.format("hh:mm:ss");
+    console.log('DEPARTURE TIME: ',formatted);
+    return formatted;
+}
+};
+
